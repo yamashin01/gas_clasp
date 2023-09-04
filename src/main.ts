67b48f39ee325ba10d7d3fd15c8d9673f64f030a
yamada <shinya.yamada@benext-corp.com>
2023-09-05 07:16:26 +0900

    ♻️ Refactor to ES6

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 type OnEditEvent = GoogleAppsScript.Events.SheetsOnEdit;
 
-function onEdit(e: OnEditEvent) {
-  // 入力を監視するセルの範囲を指定します。
-  let sheetName = "シート1";
-  let monitoredRange = "A1";
+const onEdit = (e: OnEditEvent) => {
+  // 監視元のセルを指定
+  const sheetName = "シート1";
+  const monitoredRange = "A1";
 
-  // 反映先のセルを指定します。
-  let targetSheetName = "シート1";
-  let targetRange = "B1";
+  // 反映先セルを指定
+  const targetSheetName = "シート1";
+  const targetRange = "B1";
 
-  let sheet = e.source.getSheetByName(sheetName);
-  let targetSheet = e.source.getSheetByName(targetSheetName);
+  const sheet = e.source.getSheetByName(sheetName);
+  const targetSheet = e.source.getSheetByName(targetSheetName);
 
-  // 編集されたセルが監視対象のセルかどうかを確認します。
+  // 編集されたセルが監視対象のセルかどうかを確認
   let editedRange = e.range.getA1Notation();
   if (sheetName === e.source.getSheetName() && editedRange === monitoredRange) {
-    // 監視対象のセルが編集された場合、その値を反映先のセルにコピーします。
+    // 監視対象のセルが編集された場合、その値を反映先のセルにコピー
     let value = sheet?.getRange(monitoredRange).getValue();
     targetSheet?.getRange(targetRange).setValue(value);
   }
-}
+};
